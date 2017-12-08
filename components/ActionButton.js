70df240125f2039bc619a40062dafc325f172743
jschuster <jSchuster>
2017-12-08 14:57:26 -0800

    Add Card Final

---
+++ components/ActionButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+export default function ActionButton({ onPress, styles, text, color }){
+	return (
+		<TouchableOpacity onPress={onPress}
+						  style={[styles.iosBtn, { backgroundColor: color }]}>
+			<Text style={styles.submitBtnText}>{text}</Text>
+		</TouchableOpacity>
+	)
+}
--- components/DeckView.js
+++ components/DeckView.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { getData } from '../utils/api' 
 import { connect } from 'react-redux'
+import { purple, white, red } from '../utils/colors'
+import ActionButton from './ActionButton'
 
 class DeckView extends React.Component {
 	render(){
@@ -12,6 +14,11 @@ class DeckView extends React.Component {
 			<View style={styles.container}>
 				<Text>{decks[deck].title}</Text>
 				<Text>{decks[deck].questions.length}</Text>
+
+				<ActionButton styles={styles} text={'Add Card'} color={purple}
+	        		onPress={() => this.props.navigation.navigate('AddCard', { entryId: deck })}/>
+	        	<ActionButton styles={styles} text={'Start Quiz'} color={red} 
+	        		onPress={() => this.props.navigation.navigate('Quiz', { entryId: deck })}/>
 			</View>
 		)
 	}
@@ -22,6 +29,18 @@ const styles = StyleSheet.create({
 		flex: 1, 
 		justifyContent: 'center',
 		alignItems: 'center'
+	},
+	iosBtn: {
+		padding: 10,
+		borderRadius: 7,
+		height: 45,
+		margin: 5,
+		width: 170
+	},
+	submitBtnText: {
+		color: white,
+		fontSize: 22,
+		textAlign: 'center'
 	}
 })
 
